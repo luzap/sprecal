91e485650c25c919f6386bdcf494749587eb8d29
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2017-05-25 16:40:36 +0400

    Fix merge conflict

--- sprecal/google.py
+++
@@ -1,9 +0,0 @@
-import os
-from googleapiclient import discovery
-import oauth2client
-from oauth2client import client
-from oauth2client import tools
-
-CLIENT_SECRET_FILE = "client_secret.json"
-SCOPES = "https://www.googleapis.com/auth/spreadsheets"
-APPLICATION_NAME = "Sprecal"
